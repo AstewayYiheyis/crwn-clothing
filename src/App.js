7f61b443b56bcc1b967573671d84184383400ebd
Asteway <astxi1000@gmail.com>
2021-12-31 01:46:49 -0600

    added form input component for sign in page and styled it

--- src/App.js
+++ src/App.js
@@ -4,22 +4,17 @@ import HomePage from './pages/homepage/homepage.component';
 import { Route, Routes , Link} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './components/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
-const About = (props) => {
-  console.log(props)
-  return(<h1>About Page</h1>);
-}
+
 function App() {
   return (
     <>
       <Header/>
       <Routes>
         <Route exact path="/" element={<HomePage/>} />
-        <Route path="/about" element={<About/>} />
         <Route path="/shop" element={<ShopPage/>} />
-        <Route path="/hats" element={<h1>Hats</h1>} />
-        <Route path="/jackets" element={<h1>Jackets</h1>} />
-        <Route path="/sneakers" element={<h1>Sneakers</h1>} />
+        <Route path="/signin" element={<SignInAndSignUpPage/>} /> 
       </Routes>
     </>
   );
---
+++ src/components/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './sign-in-and-sign-up.styles.scss';
+import SignIn from '../sign-in/sign-in.component';
+
+const SignInAndSignUpPage = () => (
+    <div>
+        <SignIn/>
+    </div>
+);
+
+export default SignInAndSignUpPage;
